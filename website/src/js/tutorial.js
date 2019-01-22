b0f0b13685e22ce4c353cc2b6cea7531c1a113e5
Stefan Kaltenegger <stefan@stk.works>
2019-01-23 00:04:33 +0100

    clean up tutorial html and js

--- website/src/js/tutorial.js
+++ website/src/js/tutorial.js
@@ -1,172 +1,13 @@
-// /**
-//  * tutorial.js
-//  * http://www.codrops.com
-//  *
-//  * Licensed under the MIT license.
-//  * http://www.opensource.org/licenses/mit-license.php
-//  *
-//  * Copyright 2019, Codrops
-//  * http://www.codrops.com
-//  */
-
-// let clientX = -100;
-// let clientY = -100;
-// const innerCursor = document.querySelector(".cursor--small");
-// let lastX = 0;
-// let lastY = 0;
-// let isStuck = false;
-// let showCursor = false;
-// let group, stuckX, stuckY, fillOuterCursor;
-
-// const initCursor = () => {
-//   const { paper } = window;
-//   // const unveilCursor = e => {
-//   //   group.position = new paper.Point(e.clientX, e.clientY);
-//   //   setTimeout(() => {
-//   //     0.2 = 0.2;
-//   //   }, 100);
-//   //   showCursor = true;
-//   // };
-//   // document.addEventListener("mousemove", unveilCursor);
-
-//   document.addEventListener("mousemove", e => {
-//     clientX = e.clientX;
-//     clientY = e.clientY;
-//   });
-
-//   const render = () => {
-//     // TweenMax.set(innerCursor, {
-//     //   x: clientX,
-//     //   y: clientY
-//     // });
-//     innerCursor.style.transform = `translate(${clientX}px, ${clientY}px)`;
-//     // if (showCursor) {
-//     //   document.removeEventListener("mousemove", unveilCursor);
-//     // }
-//     requestAnimationFrame(render);
-//   };
-//   requestAnimationFrame(render);
-// };
-
-// const initCanvas = () => {
-//   const { paper, SimplexNoise } = window;
-//   const canvas = document.querySelector(".cursor--canvas");
-//   const shapeBounds = {
-//     width: 75,
-//     height: 75
-//   };
-//   paper.setup(canvas);
-
-//   const strokeColor = "rgba(255, 0, 0, 0.5)";
-//   const strokeWidth = 1;
-//   const segments = 8;
-//   const radius = 15;
-//   const noiseScale = 150; // speed
-//   const noiseRange = 4; // range of distortion
-//   let isNoisy = false;
-
-//   const polygon = new paper.Path.RegularPolygon(
-//     new paper.Point(0, 0),
-//     segments,
-//     radius
-//   );
-//   polygon.strokeColor = strokeColor;
-//   polygon.strokeWidth = strokeWidth;
-//   polygon.smooth();
-
-//   group = new paper.Group([polygon]);
-//   group.applyMatrix = false;
-
-//   const noiseObjects = polygon.segments.map(() => new SimplexNoise());
-//   let bigCoordinates = [];
-
-//   const lerp = (a, b, n) => {
-//     return (1 - n) * a + n * b;
-//   };
-
-//   const map = (value, in_min, in_max, out_min, out_max) => {
-//     return (
-//       ((value - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
-//     );
-//   };
-
-//   paper.view.onFrame = event => {
-//     if (!isStuck) {
-//       // move circle around normally
-//       lastX = lerp(lastX, clientX, 0.2);
-//       lastY = lerp(lastY, clientY, 0.2);
-//       group.position = new paper.Point(lastX, lastY);
-//     } else if (isStuck) {
-//       // fixed position on a nav item
-//       lastX = lerp(lastX, stuckX, 0.2);
-//       lastY = lerp(lastY, stuckY, 0.2);
-//       group.position = new paper.Point(lastX, lastY);
-//     }
-//     if (isStuck && polygon.bounds.width < shapeBounds.width) {
-//       // scale up the shape
-//       polygon.scale(1.08);
-//     } else if (!isStuck && polygon.bounds.width > 30) {
-//       // remove noise
-//       if (isNoisy) {
-//         polygon.segments.forEach((segment, i) => {
-//           segment.point.set(bigCoordinates[i][0], bigCoordinates[i][1]);
-//         });
-//         isNoisy = false;
-//         bigCoordinates = [];
-//       }
-//       // scale down the shape
-//       const scaleDown = 0.92;
-//       polygon.scale(scaleDown);
-//     }
-
-//     // while stuck and when big, do perlin noise
-//     if (isStuck && polygon.bounds.width >= shapeBounds.width) {
-//       isNoisy = true;
-//       // first get coordinates of large circle
-//       if (bigCoordinates.length === 0) {
-//         polygon.segments.forEach((segment, i) => {
-//           bigCoordinates[i] = [segment.point.x, segment.point.y];
-//         });
-//       }
-//       // calculate noise value for each point at that frame
-//       polygon.segments.forEach((segment, i) => {
-//         const noiseX = noiseObjects[i].noise2D(event.count / noiseScale, 0);
-//         const noiseY = noiseObjects[i].noise2D(event.count / noiseScale, 1);
-//         const distortionX = map(noiseX, -1, 1, -noiseRange, noiseRange);
-//         const distortionY = map(noiseY, -1, 1, -noiseRange, noiseRange);
-//         const newX = bigCoordinates[i][0] + distortionX;
-//         const newY = bigCoordinates[i][1] + distortionY;
-//         segment.point.set(newX, newY);
-//       });
-//     }
-
-//     polygon.smooth();
-//   };
-// };
-
-// const initHovers = () => {
-//   const handleMouseEnter = e => {
-//     const navItem = e.currentTarget;
-//     const navItemBox = navItem.getBoundingClientRect();
-//     stuckX = Math.round(navItemBox.left + navItemBox.width / 2);
-//     stuckY = Math.round(navItemBox.top + navItemBox.height / 2);
-//     isStuck = true;
-//   };
-
-//   const handleMouseLeave = () => {
-//     isStuck = false;
-//   };
-
-//   const linkItems = document.querySelectorAll(".link");
-//   linkItems.forEach(item => {
-//     item.addEventListener("mouseenter", handleMouseEnter);
-//     item.addEventListener("mouseleave", handleMouseLeave);
-//   });
-// };
-
-// initCursor();
-// initCanvas();
-// initHovers();
+/**
+ * tutorial.js
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Copyright 2019, Codrops
+ * http://www.codrops.com
+ */
 
 // set the starting position of the cursor outside of the screen
 let clientX = -100;
@@ -183,13 +24,13 @@ const initCursor = () => {
   // transform the innerCursor to the current mouse position
   // use requestAnimationFrame() for smooth performance
   const render = () => {
+    innerCursor.style.transform = `translate(${clientX}px, ${clientY}px)`;
     // if you are already using TweenMax in your project, you might as well
-    // innerCursor.style.transform = `translate(${clientX}px, ${clientY}px)`;
-    // use TweenMax.set() instead of the next line
-    TweenMax.set(innerCursor, {
-      x: clientX,
-      y: clientY
-    });
+    // use TweenMax.set() instead
+    // TweenMax.set(innerCursor, {
+    //   x: clientX,
+    //   y: clientY
+    // });
 
     requestAnimationFrame(render);
   };
